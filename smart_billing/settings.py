7980389bd76a8505601e934fab1d244b00b73a35
Dell <Dell@DESKTOP-L5EO33D>
2026-05-26 23:04:10 +0530

    fix apps indentation

--- smart_billing/settings.py
+++ smart_billing/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "change-this-in-production")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
