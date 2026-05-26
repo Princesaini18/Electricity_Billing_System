d75de7d02257b95f7c22326a66169a0c32586c2e
Dell <Dell@DESKTOP-L5EO33D>
2026-05-26 22:30:18 +0530

    admin

--- billing/apps.py
+++ billing/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class BillingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "billing"
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="admin123"
+            )
--- smart_billing/settings.py
+++ smart_billing/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "billing",
+    "billing.apps.BillingConfig",
 ]
 
 MIDDLEWARE = [
